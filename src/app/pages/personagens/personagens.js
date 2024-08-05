503de89f7d0e77f3fe89910713caffd7976ab58b
JhuliaFermoFascin <jhuliafermo@gmail.com>
2024-08-05 17:08:30 -0300

    terminei

---
+++ src/app/pages/personagens/personagens.js
@@ -0,0 +1,198 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttonAlex = document.getElementById('Alex');
+    const buttonElliot = document.getElementById('Elliot');
+    const buttonHarvey = document.getElementById('Harvey');
+    const buttonSam = document.getElementById('Sam');
+    const buttonSebastian = document.getElementById('Sebastian');
+    const buttonShaine = document.getElementById('Shane');
+    const buttonAbigail = document.getElementById('Abigail');
+    const buttonEmily = document.getElementById('Emily');
+    const buttonHailey = document.getElementById('Haley');
+    const buttonLeah = document.getElementById('Leah');
+    const buttonMaru = document.getElementById('Maru');
+    const buttonPenny = document.getElementById('Penny');
+    const buttonAnao = document.getElementById('Anao');
+    const buttonCaroline = document.getElementById('Caroline');
+    const buttonClint = document.getElementById('Clint');
+    const buttonDemetrius = document.getElementById('Demetrius');
+    const buttonEvelyn = document.getElementById('Evelyn');
+    const buttonFeiticeiro = document.getElementById('Feiticeiro');
+    const buttonGeorge = document.getElementById('George');
+    const buttonGus = document.getElementById('Gus');
+    const buttonJas = document.getElementById('Jas');
+    const buttonKent = document.getElementById('Kent');
+    const buttonKrobus = document.getElementById('Krobus');
+    const buttonLeo = document.getElementById('Leo');
+    const buttonLewis = document.getElementById('Lewis');
+    const buttonLinus = document.getElementById('Linus');
+    const buttonMarnie = document.getElementById('Marnie');
+    const buttonPam = document.getElementById('Pam');
+    const buttonPierre = document.getElementById('Pierre');
+    const buttonRobin = document.getElementById('Robin');
+    const buttonSandy = document.getElementById('Sandy');
+    const buttonAntony = document.getElementById('Vincent');
+    const buttonWilly = document.getElementById('Willy');
+    const buttonAjudante = document.getElementById('Ajudante');
+    const buttonAvo = document.getElementById('Avo');
+    const buttonPassarinha = document.getElementById('Passarinha');
+    const buttonGil = document.getElementById('Gil');
+    const buttonGovernador = document.getElementById('Governador');
+    const buttonGunther = document.getElementById('Gunther');
+    const buttonMarlon = document.getElementById('Marlon');
+    const buttonMorris = document.getElementById('Morris');
+    const buttonProfessorCaracol = document.getElementById('ProfessorCaracol');
+    const buttonSeguranca = document.getElementById('Seguranca');
+    const buttonSrQi = document.getElementById('SrQi');
+    const linkAertefato = document.getElementById('linkArtefatos');
+    const linkMineral = document.getElementById('linkMinerais');
+    const linkAnimais = document.getElementById('linkAnimais');
+    const linkLavoura = document.getElementById('linkLavouras');
+
+    buttonAlex.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonElliot.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonHarvey.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonSam.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonSebastian.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonShaine.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonAbigail.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonEmily.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonHailey.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    buttonMaru.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonPenny.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonAnao.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonCaroline.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonClint.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonDemetrius.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonEvelyn.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonFeiticeiro.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonGeorge.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    buttonGus.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonJas.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonKent.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonKrobus.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonLeo.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonLewis.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonLinus.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonMarnie.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonPam.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    buttonPierre.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonRobin.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonSandy.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonAntony.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonWilly.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonAjudante.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonAvo.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonPassarinha.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    buttonLeah.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    buttonGil.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonGovernador.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonAntony.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonGunther.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonMarlon.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonMorris.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    buttonProfessorCaracol.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    buttonSeguranca.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    buttonSrQi.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    linkAertefato.addEventListener('click', function() {
+        alert('Página temporariamente indisponível!');
+    });
+    linkMineral.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    linkAnimais.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+    linkLavoura.addEventListener('click', function(){
+        alert('Página temporariamente indisponível!');
+    });
+});
